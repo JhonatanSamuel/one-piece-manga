62cd333af653386abd198a920d7094eacd5aebbb
JhonatanSamuel <jhonatsamuel@gmail.com>
2025-03-09 18:57:20 -0300

    acrescentando img

--- src/components/ListaCapitulos.js
+++ src/components/ListaCapitulos.js
@@ -3,6 +3,9 @@ import React from 'react';
 function ListaCapitulos({ mangas, selecionarCapitulo }) {
     return (
         <div>
+            <img className='logo' src='./img/luffy-logo.jpg'  alt='img-logo' />
+            <img className='capa' src='./img/onepiece-logo.jpg' alt='img-capa' />
+
             <h2>Capitulo 1108 em diante ... </h2>
             <ul>
                 {mangas.map((manga, index) => (

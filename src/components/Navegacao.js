07451ed4a53cb581737c47d7722074f307842e54
JhonatanSamuel <jhonatsamuel@gmail.com>
2025-03-09 20:17:02 -0300

    ,

--- src/components/Navegacao.js
+++ src/components/Navegacao.js
@@ -1,29 +1,12 @@
 import React from 'react';
 
 function Navegacao({ capituloAtual, totalCapitulos, proximoCapitulo, capituloAnterior }) {
-    
-    const handleProximo = () => {
-        if (capituloAtual < totalCapitulos - 1) {
-            proximoCapitulo();
-        } else {
-            alert("🚨 Último capítulo disponível!");
-        }
-    };
-
-    const handleAnterior = () => {
-        if (capituloAtual > 0) {
-            capituloAnterior();
-        } else {
-            alert("🚨 Primeiro capítulo!");
-        }
-    };
-    
     return (
         <div className="navegacao">
-            <button onClick={handleProximo} disabled={capituloAtual === totalCapitulos - 1}>
-                Capítulo Anterior 
+            <button onClick={capituloAnterior} disabled={capituloAtual === totalCapitulos - 1}>
+                Capítulo Anterior
             </button>
-            <button onClick={handleAnterior } disabled={capituloAtual === 0}>
+            <button onClick={proximoCapitulo} disabled={capituloAtual === 0}>
                 Próximo Capítulo
             </button>
         </div>

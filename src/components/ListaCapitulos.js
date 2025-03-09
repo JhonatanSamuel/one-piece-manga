d15c04b8ab72f26515747fd4228912f5ef395406
JhonatanSamuel <jhonatsamuel@gmail.com>
2025-03-09 17:28:20 -0300

    botoes

--- src/components/ListaCapitulos.js
+++ src/components/ListaCapitulos.js
@@ -3,11 +3,11 @@ import React from 'react';
 function ListaCapitulos({ mangas, selecionarCapitulo }) {
     return (
         <div>
-            <h2>Escolha um capítulo</h2>
+            <h2>Capitulo 1108 em diante ... </h2>
             <ul>
                 {mangas.map((manga, index) => (
-                    <li key={manga.id}>
-                        <button onClick={() => selecionarCapitulo(index)}>
+                    <li className='voltarParaLista' key={manga.id}>
+                        <button  onClick={() => selecionarCapitulo(index)}>
                             {manga.titulo}
                         </button>
                     </li>
